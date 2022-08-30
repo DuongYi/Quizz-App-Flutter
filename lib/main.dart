a54349cd220f674ca3fc2aadacb8cee790a639eb
Duong Yi <duonguet1903@gmail.com>
2022-08-30 17:36:05 +0700

    Pasing Callback Function  Around

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
@@ -40,20 +37,9 @@ class _MyAppState extends State<MyApp> {
         body: Column(
           children: [
             Question(questions[_questionIndex]),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: (() => print('Answer 2 chosen!')),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: (() {
-                print('Answer 3 chosen!');
-              }),
-            ),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),
       ),
